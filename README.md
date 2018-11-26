5a313a017fa6df7090eca65597704c78338556c4
LukasPol <lukaspaespol@gmail.com>
2018-11-26 18:09:17 -0300

    commit #1

---
+++ README.md
@@ -0,0 +1,45 @@
+# CRUD para gastos mensal
+
+Criado para aprender o Django, como ele funciona, como usar variaveis no HTML, e outras coisas
+
+### Para Rodar
+
+- Crie e inicie a Máquina Virtual
+
+Windows
+```
+python -m venv <nome>
+
+. <nome>/Scripts/activate
+```
+
+Linux e Mac
+```
+python3 -m venv <nome> 
+
+<nome>/Scripts/activate
+```
+
+- Instalar o Django
+
+```
+pip install django
+```
+
+- Criar o Bando de Dados
+
+```
+python manage.py migrate
+```
+
+- Rodar 
+
+´´´
+python manage.py mrunserver
+´´´
+
+- Atualizar BD
+
+````
+python manage.py makemigrations
+```
---
+++ contas/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from .models import Transacao
+from .form import TransacaoForm
+import datetime
+
+
+def home(request):
+    data = {}
+    data['transacao'] = ['t1','t2', 't3']
+
+    data['now'] = datetime.datetime.now() # pegar a hora naquela momento
+    # html = '<html><body>Olá, agora é %s</body></html>' %now
+    # return HttpResponse(html)
+
+    return render(request, 'contas/home.html',data)
+
+def listagem(request):
+    data = {}
+    data['transacoes'] = Transacao.objects.all()
+
+    return render(request, 'contas/listagem.html',data)
+
+def nova_transacao(request):
+    data = {}
+    form = TransacaoForm(request.POST or None) # ele irá verificar se tem alguma coisa e já preenche
+
+    if form.is_valid(): #verifica se é valido o formulário
+        form.save()
+        #return listagem(request) # quando salvar a página retorna para a listagem, mas replica os dados
+        return redirect('url_list') # para nn replicar os dados
+
+    data['form'] = form #mesma coisa
+    return render(request, 'contas/form.html', data)
+
+
+def update(request, pk):
+    transacao = Transacao.objects.get(pk=pk) # filter para retornar maisde um objeto
+    form = TransacaoForm(request.POST or None, instance=transacao)
+    # para não começar o form vazio
+
+    if form.is_valid(): 
+        form.save()
+        return redirect('url_list')
+    data = {}
+    data['form'] = form 
+    data['transacao']  = transacao #para deletar, pois lánn tem a variavel
+    return render(request, 'contas/form.html', data)
+
+def delete(request, pk):
+    transacao = Transacao.objects.get(pk=pk)
+    transacao.delete()
+    return redirect('url_list')
